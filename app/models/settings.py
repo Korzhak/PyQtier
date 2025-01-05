9d943ee084ac20e47868384ee48789ad191393bc
Korzhak <navchalkin@ex.ua>
2025-01-05 21:51:05 +0200

    made python module structure. Posted it on test pypi

--- app/models/settings.py
+++
@@ -1,6 +0,0 @@
-from core.models import PyQtierSettingsModel
-
-
-class SettingsModel(PyQtierSettingsModel):
-    def __init__(self, settings_id: str = ""):
-        super(SettingsModel, self).__init__(settings_id)
