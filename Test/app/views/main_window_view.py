90d6e8936069c50e0b6ea385fe592fc0a9793c59
Богдан Коржак <korzhak-bm@dvluch.loc>
2025-01-07 17:09:14 +0200

    removed trash

--- Test/app/views/main_window_view.py
+++
@@ -1,6 +0,0 @@
-from pyqtier.views import PyQtierMainWindowView
-
-
-class MainWindowView(PyQtierMainWindowView):
-    def add_behaviour(self):
-        self.ui.actionSettings.triggered.connect(self.get_callback('settings_widget'))
