143c11b6437e2f44080f50894cc44d1e99ea493c
Korzhak <bohdan.korzhak@gmail.com>
2025-02-15 22:22:33 +0200

    changed name of company in config

--- src/pyqtier/scripts/generator_templates/config_templates.py
+++ src/pyqtier/scripts/generator_templates/config_templates.py
@@ -3,10 +3,10 @@ APP_VERSION = "0.0.1"
 
 # Set your company and app names for creating About window
 APP_NAME = "PyQtier"
-COMPANY_NAME = "Korzhak"
+COMPANY_NAME = "PyQtier Inc."
 
 # Set your company and app short names for autosaving parameters of application to registry.
 APP_SHORT_NAME = "PyQtier"
-COMPANY_SHORT_NAME = "Korzhak"
+COMPANY_SHORT_NAME = "PyQtierInc"
 
 """
