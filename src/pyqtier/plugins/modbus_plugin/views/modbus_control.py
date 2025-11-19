a1e2ace319000565c12446d60dc82e627f0f55ac
Yurii Solodzhuk <solodzhuk-ym@dvluch.loc>
2025-11-19 14:53:34 +0200

    added modbus plugin

---
+++ src/pyqtier/plugins/modbus_plugin/views/modbus_control.py
@@ -0,0 +1,50 @@
+# views/modbus_control_with_baudrate.py
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtCore, QtWidgets
+
+
+class Ui_ModbusWidget(object):
+    def setupUi(self, ModbusWidget):
+        ModbusWidget.setObjectName("ModbusWidget")
+        ModbusWidget.resize(500, 88)
+
+        self.horizontalLayout = QtWidgets.QHBoxLayout(ModbusWidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+
+        self.frame = QtWidgets.QFrame(ModbusWidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+
+        self.gridLayout = QtWidgets.QGridLayout(self.frame)
+        self.gridLayout.setObjectName("gridLayout")
+
+        # COM Port
+        self.cb_serial_ports = QtWidgets.QComboBox(self.frame)
+        self.cb_serial_ports.setObjectName("cb_serial_ports")
+        self.gridLayout.addWidget(self.cb_serial_ports, 0, 0, 1, 1)
+
+        # Slave ID
+        self.spinbox_slave_id = QtWidgets.QSpinBox(self.frame)
+        self.spinbox_slave_id.setMinimum(1)
+        self.spinbox_slave_id.setMaximum(247)
+        self.spinbox_slave_id.setValue(1)
+        self.spinbox_slave_id.setPrefix("ID: ")
+        self.spinbox_slave_id.setObjectName("spinbox_slave_id")
+        self.gridLayout.addWidget(self.spinbox_slave_id, 0, 2, 1, 1)
+
+        # Connect Button
+        self.bt_connect_disconnect = QtWidgets.QPushButton(self.frame)
+        self.bt_connect_disconnect.setObjectName("bt_connect_disconnect")
+        self.gridLayout.addWidget(self.bt_connect_disconnect, 0, 3, 1, 1)
+
+        self.horizontalLayout.addWidget(self.frame)
+
+        self.retranslateUi(ModbusWidget)
+        QtCore.QMetaObject.connectSlotsByName(ModbusWidget)
+
+    def retranslateUi(self, ModbusWidget):
+        _translate = QtCore.QCoreApplication.translate
+        ModbusWidget.setWindowTitle(_translate("ModbusWidget", "Modbus"))
+        self.bt_connect_disconnect.setText(_translate("ModbusWidget", "Connect"))
